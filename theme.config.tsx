bc1752c5c54d619df900c2d876944acd78e805f2
heroisuseless <3256384696@qq.com>
2024-04-23 11:08:44 +0800

    fix: kk

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,18 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="前端面试题" />
+      <meta property="og:description" content="前端面试题" />
+    </>
+  ),
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – 前端面试题'
+    }
+  },
   logo: <span>前端面试题</span>,
   project: {
     link: 'https://github.com/HeroIsUseless/docnext',
