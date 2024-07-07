b9a43547ca975b361269887c579ca70a1e4aa45d
heroisuseless <3256384696@qq.com>
2024-07-07 22:58:36 +0800

    fix: robot

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,12 @@ module.exports = {
   siteUrl: "https://front-interview.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  exclude: ['/1_internet', '/2_htmljscss/*', '/3_frame/*', '/4_writeq/*', '/5_design', '/6_project'],
+  robotsTxtOptions: {
+    policies: [{ 
+      userAgent: '*', 
+      allow: '/',
+      disallow: ['/1_internet', '/2_htmljscss/', '/3_frame/', '/4_writeq/', '/5_design', '/6_project'] 
+    }]
+  },
 };
