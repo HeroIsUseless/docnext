df5b50dd5ae04189963f3872d27eae3f732012d8
heroisuseless <3256384696@qq.com>
2024-07-08 12:59:13 +0800

    fix: front

--- components/counters.tsx
+++ components/counters.tsx
@@ -1,10 +1,18 @@
 // Example from https://beta.reactjs.org/learn
 
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import styles from './counters.module.css'
+import metaJson from '../pages/question/_meta.json'
 
 function MyButton() {
   const [count, setCount] = useState(0)
+  const array = useMemo(() => {
+    const res = []
+    Object.values(metaJson).forEach((item) => {
+      res.push(item)
+    })
+    return res
+  }, [metaJson])
 
   function handleClick() {
     setCount(count + 1)
@@ -12,9 +20,14 @@ function MyButton() {
 
   return (
     <div>
-      <button onClick={handleClick} className={styles.counter}>
-        Clicked {count} times
-      </button>
+      <h2 style={{fontSize: '24px', fontWeight: 'bold', marginTop: '32px', marginBottom: '24px'}}>
+        最新面试题👇
+      </h2>
+      {Object.entries(metaJson).map((item, index) => {
+        return <p style={{margin: '12px'}} key={index}>
+          <a style={{color: '#1678ff'}} href={'question/'+item[0]}>{item[1]}</a>
+        </p>
+      })}
     </div>
   )
 }
